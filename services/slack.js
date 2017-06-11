92ab39cea5ca25d6facbfe63049381f9fd2145c0
Kai Hao <kevin830726@gmail.com>
2017-06-11 22:21:33 +0800

    add prs

--- services/slack.js
+++ services/slack.js
@@ -23,7 +23,7 @@ const formatMessage = (lastCoverage, coverage) => {
 
 const sendSlackWebhook = (webhook, lastCoverage, coverage, others = {}) => {
   const { emoji, text } = formatMessage(lastCoverage, coverage);
-  const { vcs, username, project, branch, ignoreSame } = others;
+  const { vcs, username, project, branch, ignoreSame, prs } = others;
   let body = text;
 
   // bypass slack if the coverage remained the same
@@ -36,6 +36,22 @@ const sendSlackWebhook = (webhook, lastCoverage, coverage, others = {}) => {
     const repo = `<https://${vcs}.com/${username}/${project}/${branch ? `/tree/${branch}` : ''}|[${username}/${project}]>`;
     const branchSuffix = branch ? ` on <${repoURL}/tree/${branch}|${branch}>` : '';
     body = `<${repoURL}|[${username}/${project}]> ${text}${branchSuffix}`;
+  }
+
+  if (prs) {
+    const PR_REGEX = /\/(\d+)\/?$/g;
+    const prsLinks = prs.split(',').map((pr) => {
+      const link = pr.trim();
+
+      if (link) {
+        const group = PR_REGEX.exec(link);
+        const prNum = group ? group[1] : null;
+        PR_REGEX.lastIndex = 0;
+        return prNum !== null ? `<${link}|#${prNum}>` : '';
+      }
+    }).filter(Boolean);
+
+    body += ` (${prsLinks.join(',')})`;
   }
 
   return fetch(webhook, {
