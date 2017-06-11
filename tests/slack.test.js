88b9756c87ad1562a46f8e362957fb2cf309a93f
Kai Hao <kevin830726@gmail.com>
2017-06-11 21:37:04 +0800

    update tests

--- tests/slack.test.js
+++ tests/slack.test.js
@@ -29,8 +29,33 @@ describe('formatMessage', () => {
 });
 
 describe('sendSlackWebhook', () => {
+  afterEach(() => {
+    fetch.mockClear();
+  });
+
   it('should call fetch with parameters', async () => {
     await sendSlackWebhook('slack', 50, 100);
+
+    expect(fetch.mock.calls).toMatchSnapshot();
+  });
+
+  it('should format the text if have other options', async () => {
+    await sendSlackWebhook('slack', 50, 100, {
+      username: 'usernmame',
+      project: 'project',
+      vcs: 'github',
+    });
+
+    expect(fetch.mock.calls).toMatchSnapshot();
+  });
+
+  it('should further format the text if have branch', async () => {
+    await sendSlackWebhook('slack', 50, 100, {
+      username: 'usernmame',
+      project: 'project',
+      vcs: 'github',
+      branch: 'branch',
+    });
 
     expect(fetch.mock.calls).toMatchSnapshot();
   });
